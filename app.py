c21befab8adc550ca26912615519deadd184cc32
milonpabis <miloszpabis@onet.pl>
2024-07-16 13:58:13 +0200

    Flask integration with database

--- app.py
+++ app.py
@@ -27,23 +27,30 @@ def home():
     return render_template("mainpage.html")
 
 
-@app.route("/performance/")
-def performance():
+@app.route("/performance/<period>/")
+def performance(period):
+    print("PERIOD", period)
     db = get_engine().modelDB
     performance_rf = db.get_model_performance("RandomForest")
     performance_ab = db.get_model_performance("AdaBoost")
     performance_gb = db.get_model_performance("GradientBoost")
     
-    fig_rf = GraphBTC("Random Forest", performance_rf, ["total"]).get_graph()
-    fig_ab = GraphBTC("Ada Boost", performance_ab, ["total"]).get_graph()
-    fig_gb = GraphBTC("Gradient Boost", performance_gb, ["total"]).get_graph()
+    fig_rf = GraphBTC("Random Forest", performance_rf, [period]).get_graph()
+    fig_ab = GraphBTC("Ada Boost", performance_ab, [period]).get_graph()
+    fig_gb = GraphBTC("Gradient Boost", performance_gb, [period]).get_graph()
 
     return render_template("performance.html", rf_graph=fig_rf.to_json(), ab_graph=fig_ab.to_json(), gb_graph=fig_gb.to_json())
 
 
 @app.route("/models/")
 def models():
-    return render_template("models.html")
+    engine = get_engine()
+    pred = engine.get_prediction_today()
+    
+    return render_template("models.html",
+                            gb_pred = pred["GradientBoost"],
+                            ab_pred = pred["AdaBoost"],
+                            rf_pred = pred["RandomForest"])
 
 
 @app.route("/about/")
